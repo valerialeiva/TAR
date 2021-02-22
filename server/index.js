32ea5dd958874af4bfb9c8e041caac431c9b9d2d
valerialeiva <Suisivale30>
2021-02-21 21:39:33 -0600

    Revert "User"
    This reverts commit 6df389f8f438ad574682311f0710a630af4d24bb.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 
 const db = require('./db')
 const movieRouter = require('./routes/movie-router')
-const userRouter = require('./routes/user-router')
 
 const app = express()
 const apiPort = 3000
@@ -20,6 +19,5 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', movieRouter)
-app.use('/api', userRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
